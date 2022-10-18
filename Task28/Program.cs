0bb6510d19da981da17428abeec0b6016184e13f
Lisa Perlova <libertas.lis@yandex.ru>
2022-10-18 21:42:12 +0500

    added tasks 24, 26, 28, 30

---
+++ Task28/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+long number = Convert.ToInt64(Console.ReadLine());
+long Factorial (long num)
+{
+    long factorial = 1;
+    int i;
+    for (i = 1; i <= num; i++)
+    {
+        try
+        {
+            checked
+            {
+                factorial = factorial * i;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Переполнение типа!");
+            break;
+        }
+        
+    }
+    Console.WriteLine($"Факториал числа {i - 1} -> {factorial}");
+    return factorial;
+}
+long fact = Factorial (number);
+// Console.WriteLine($"Произведение чисел от 1 до {number} -> {fact}");
