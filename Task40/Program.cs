4f12c2b235a055018ecdc8bbd363dd7799c2bd5f
Lisa Perlova <libertas.lis@yandex.ru>
2022-10-24 10:45:40 +0500

    added tasks for seminar 6

---
+++ Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+Console.WriteLine("Напишите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle (int num1, int num2, int num3)
+{
+    return (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num2 + num1);
+}
+bool triangle = Triangle(number1, number2, number3);
+if (triangle) Console.WriteLine("Такой треугольник может существовать");
+else Console.Write("Такой треугольник НЕ может существовать");
---
+++ Task45/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования. 
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] Fibonacci(int number)
+{
+    int[] array = new int[number];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = Fibonacci(num);
+PrintArray(arr);
+int[] newArr = CopyArray(arr);
+PrintArray(newArr);
+
+
+// Второй варинат:
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// void Fibonacci(int num)
+// {
+//     int firstNumber = 0;
+//     int secondNumber = 1;
+//     Console.Write($"{firstNumber} {secondNumber} ");
+//     for (int i = 0; i < num - 2; i++)
+//     {
+//         int nextNumber = firstNumber + secondNumber;
+//         Console.Write($"{netNumber}");
+//         firstNumber = secondNumber;
+//         secondNumber = nextNumber;
+//     }
+// }
+
+// Fibonacci(number);
