89ca37c9b6db40274c38df4d4fec1a989adeed7b
Lisa Perlova <libertas.lis@yandex.ru>
2022-10-27 20:22:54 +0500

    seminar 7

---
+++ Task51/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int DiagonalSum(int[,] matrix)
+{
+    int sum = default;
+    int demention = 0;
+    if (matrix.GetLength(0) > matrix.GetLength(1)) demention = 1;
+    for (int i = 0; i < matrix.GetLength(demention); i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(5, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+int diagonalSum = DiagonalSum(array2D);
+Console.WriteLine(diagonalSum);
