6cc7fc942de9e60a4cd24802c7b357273aac8f00
Lisa Perlova <libertas.lis@yandex.ru>
2022-11-27 23:08:32 +0500

    add tasks 60

---
+++ Task56/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int indexRow = 0;
+int minsum = Int32.MaxValue;
+void SumRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+            if (sum < minsum)
+            {
+                minsum = sum;
+                indexRow++;
+            }
+        }
+    }
+
+
+int[,] array2D = CreateMatrixRndInt(4, 3, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+SumRows(array2D);
+Console.WriteLine($"Наименьшая сумма элементов в строке: {indexRow}, с суммой элементов: {minsum}");
---
+++ Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] DivMatrix(int[,] matrix1, int[,] matrix2)
+{
+    var matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    if (matrix1.GetLength(1) == matrix2.GetLength(0))
+    {
+        for (int i = 0; i < matrix3.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix3.GetLength(1); j++)
+            {
+                matrix3[i, j] = 0;
+                for (int n = 0; n < matrix1.GetLength(1); n++)
+                {
+                    matrix3[i, j] += matrix1[i, n] * matrix2[n, j];
+                }
+            }
+        }
+    }
+    return matrix3;
+}
+
+
+int[,] matrix1 = CreateMatrixRndInt(3, 3, 1, 9);
+int[,] matrix2 = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+PrintMatrix(DivMatrix(matrix1, matrix2));
